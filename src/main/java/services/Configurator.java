7cf8103b8e3f5bfd0769f38baa06fc8ed28b9ad1
Ovsiuk Maksim <ovsuk.maksim@gmail.com>
2019-10-02 07:51:00 +0200

    added changes for run under Selenide

--- src/main/java/services/Configurator.java
+++ src/main/java/services/Configurator.java
@@ -3,7 +3,10 @@ package services;
 public class Configurator {
 
     private static final String WEB_DRIVER_TYPE = "chrome";
-    private static final String ELECTRON_DEMO_APP_PATH = "c:\\Repo\\electron\\electron-demo-app\\Electron API Demos.exe";
+    private static final String ELECTRON_DEMO_APP_PATH = "C:\\Users\\Professional\\AppData\\Local\\Programs\\electron-react-boilerplate\\Raccoon.exe";
+
+    //C:\Users\Professional\AppData\Local\Programs\electron-react-boilerplate\Raccoon.exe
+    //c:\Repo\electron\electron-demo-app\Electron API Demos.exe
 
     public static String getWebDriverType() {
         return WEB_DRIVER_TYPE;
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,48 +6,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.winium.WiniumDriver;
 import services.DriverProvider;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
 
 public class BaseTest {
 
-    //WebDriver driver;
-
     @BeforeEach
     public void setUP(){
-        //this.driver = DriverProvider.getDriver();
         WebDriverRunner.setWebDriver(DriverProvider.getDriver());
-        Config
     }
 
     @AfterEach
     public void TearDown(){
-        //driver.close();
+        WebDriverRunner.getWebDriver().close();
     }
 
     @Test
     @DisplayName("First test")
     public void testLaunchApplication(){
         System.out.println("Hello World Raccoon");
-        open();
-        $("#using-window-events-demo-toggle").click();
-        $(By.xpath("(//div[@class='demo'])[3]//span")).waitUntil(Condition.visible, 10000).getText();
+
         System.out.println(
-                $(By.xpath("(//div[@class='demo'])[3]//span"))
-                .waitUntil(Condition.visible, 10000).getText()
+                $(byText("SIGN IN"))
+                .waitUntil(Condition.visible, 10000)
         );
 
 
 
-        System.out.println("click");
+
 
 
     }
